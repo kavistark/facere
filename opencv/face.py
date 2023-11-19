14eabb8471643bbf3397a6993f140b4ce3c63ef3
code of god kavi <73168926+kavistark@users.noreply.github.com>
2023-11-19 18:24:36 +0530

    Add files via upload

---
+++ opencv/face.py
@@ -0,0 +1,16 @@
+#www.pipy.com  pip install opencv 
+import cv2
+path="haarcascade_frontalcatface_extended.xml"
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + path)
+video_capture = cv2.VideoCapture(0)  
+while True:
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
